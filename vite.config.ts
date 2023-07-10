40f4505e7bdda0363b65320cfa9a1fb748528ab5
Komori Yuichiro <mail.cmmmli@gmail.com>
2023-07-10 17:51:19 +0900

    chore: add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      { find: "@styled-system/", replacement: `${__dirname}/styled-system/` },
+    ],
+  },
   plugins: [react()],
-})
+});
